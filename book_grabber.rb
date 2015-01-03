97119639c3fa123029f309f3da168a29037be9c3
Nat Welch <nat@natwelch.com>
2015-01-03 22:06:33 +0000

    shrink

--- book_grabber.rb
+++ book_grabber.rb
@@ -6,12 +6,7 @@ class BookGrabber
     # https://www.goodreads.com/api#reviews.list
     url = "https://www.goodreads.com/review/list?format=xml&v=2&id=#{user_id}&key=AtaRYksgV1LDWkg2dTOg&per_page=200&shelf=to_read"
     uri = URI.parse url
-    uri.open do |file|
-      body = file.read
-      p body
-      p file.meta
-      document = Nokogiri::XML(body)
-      p document
-    end
+    document = Nokogiri::XML(uri.read.split("\n").map {|l| l.strip }.join '')
+    p document.xpath("//book")
   end
 end
