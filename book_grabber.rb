b96c0c06d9f2db509f2ea42f8aee73b239125406
Nat Welch <nat@natwelch.com>
2015-01-03 22:23:34 +0000

    one way

--- book_grabber.rb
+++ book_grabber.rb
@@ -7,6 +7,7 @@ class BookGrabber
     url = "https://www.goodreads.com/review/list?format=xml&v=2&id=#{user_id}&key=AtaRYksgV1LDWkg2dTOg&per_page=200&shelf=to_read"
     uri = URI.parse url
     document = Nokogiri::XML(uri.read.split("\n").map {|l| l.strip }.join '')
-    p document.xpath("//book")
+    titles = document.xpath("//book/title").map {|t| t.content }
+    ids = document.xpath("//book/id").map {|t| t.content }
   end
 end
