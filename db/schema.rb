1de7db6f2d1548eb24b2527857208ba728b641ec
Nat Welch <nat@natwelch.com>
2015-01-04 21:32:27 +0000

    fix table name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150104210714) do
+ActiveRecord::Schema.define(version: 20150104213148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,11 @@ ActiveRecord::Schema.define(version: 20150104210714) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "authors_books", id: false, force: :cascade do |t|
+    t.integer "books_id"
+    t.integer "authors_id"
+  end
+
   create_table "books", force: :cascade do |t|
     t.text     "title"
     t.datetime "date_added"
@@ -29,11 +34,6 @@ ActiveRecord::Schema.define(version: 20150104210714) do
     t.string   "image_url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "books_authors", id: false, force: :cascade do |t|
-    t.integer "books_id"
-    t.integer "authors_id"
   end
 
   create_table "books_genres", id: false, force: :cascade do |t|
